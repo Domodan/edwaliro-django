21e1e8499ca73085598227cde2e2e85a46468437
Domodan <ongomdaniel9@gmail.com>
2022-01-26 16:27:15 +0300

    modified for deployment

--- Dockerfile
+++ Dockerfile
@@ -10,13 +10,24 @@ WORKDIR /app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+# install psycopg2 dependencies
+# RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+
 # install dependencies
 RUN pip install --upgrade pip
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+# copy entrypoint.sh
+# COPY ./entrypoint.sh .
+# RUN sed -i 's/\r$//g' /app/entrypoint.sh
+# RUN chmod +x /app/entrypoint.sh
+
 # copy project
 COPY . .
+
+# run entrypoint.sh
+# ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
 
 # CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
 
--- edwaliro/settings.py
+++ edwaliro/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%#qut06*mrow#1vkae0+h$zi&qjtf@jhn3hxd#*d=r3y8&^-wc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = [ '0.0.0.0', '127.0.0.1', ]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -90,6 +91,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # "USER": os.environ.get("SQL_USER", "user"),
+        # "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        # "HOST": os.environ.get("SQL_HOST", "localhost"),
+        # "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
